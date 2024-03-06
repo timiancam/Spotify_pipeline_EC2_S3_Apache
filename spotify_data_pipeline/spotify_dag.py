277486274e3f6047a60a8cdda711e6af74c6798d
Timothy <timiancam@gmail.com>
2024-03-06 18:33:49 +0000

    Delete subfolder

--- spotify_data_pipeline/spotify_dag.py
+++
@@ -1,7 +0,0 @@
-from datetime import timedelta
-from airflow import DAG
-from airflow.utils.dates import days_ago
-from datetime import datetime
-from spotify_etl import main
-from airflow.operators.python import PythonOperator
-
